528dfbf88f81a8b0fc03c9dc98a2da2b973504c9
sDucker <scri0962@gmail.com>
2025-06-10 18:45:03 -0600

    commit de las 8

--- src/controllers/ContactManager.java
+++ src/controllers/ContactManager.java
@@ -1,5 +1,35 @@
 package controllers;
 
+import models.Contact;
+import models.LinkedList;
+
 public class ContactManager {
-    
+    private LinkedList<Contact<String, String>> contacts;
+
+    public ContactManager() {
+        this.contacts = new LinkedList<>();
+    }
+
+    public void addContact(Contact<String, String> contact) {
+        contacts.appendToTail(contact);
+    }
+
+    public Contact<String, String> findContactByName(String name) {
+        Contact<String, String> temp = new Contact<>(name, "");
+        return contacts.findByValue(temp);
+    }
+
+    public void deleteContactByName(String name) {
+        Contact<String, String> temp = new Contact<>(name, "");
+        contacts.deleteByValue(temp);
+    }
+
+    public void printList() {
+        if (contacts.getSize() == 0) {
+            System.out.println("No contacts available.");
+            return;
+        }
+        System.out.println("Contact List:");
+        contacts.print();
+    }
 }
