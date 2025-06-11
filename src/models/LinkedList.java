dd5d23a66465b71e81ed4eec2e4e342c87d31123
Juan <juandajimenezcriollo@gmail.com>
2025-06-10 19:26:01 -0500

    Trabajo

--- src/models/LinkedList.java
+++ src/models/LinkedList.java
@@ -1,5 +1,41 @@
 package models;
 
-public class LinkedList {
+public class LinkedList<T> {
+    
+    private Node<T> head;
+    private int size;
+
+    public void appendToTail(T value) {
+
+    }
+
+    public T findByValue(T value) {
+
+    }
+
+    public void deleteByValue(T value) {
+
+    }
+
+    public void print() {
+
+    }
+
+    public Node<T> getHead() {
+        return head;
+    }
+
+    public void setHead(Node<T> head) {
+        this.head = head;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
     
 }
